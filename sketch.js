fdb39407b916a211adbd6946a6c19a3e1e87a78d
vihhguima16 <vitoria.guimaraes.silva@escola.pr.gov.br>
2024-09-27 10:46:19 -0300

    adicionando o sketch.js

---
+++ sketch.js
@@ -0,0 +1,33 @@
+let cor;
+let posicaoHorizontal;// x
+let possicaoVertical;// y
+
+function setup() {
+  createCanvas(400, 400);
+  background(color(100,0,0));
+  cor= color(random(0,255),random(0,255),random(0,255));
+  posicaoHorizontal=200
+  posicaoVertical=200
+}
+
+  function draw() {
+ fill(cor)
+    circle(posicaoHorizontal,posicaoVertical,50);
+    if(mouseX<posicaoHorizontal){
+      posicaoHorizontal=posicaoHorizontal-1;
+    }
+    if(mouseX>posicaoHorizontal){
+      posicaoHorizontal=posicaoHorizontal+1;
+    }
+    if(mouseY<posicaoVertical){
+      posicaoVertical--;
+    }
+    if(mouseY>posicaoVertical){
+      posicaoVertical++;
+      
+    }
+    
+    if (mouseIsPressed){
+      cor = color(ramdom(0,255), ramdom(0,255), ramdom(0,255), ramdom(0,100));
+    }
+}
